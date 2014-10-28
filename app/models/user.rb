9f126911333546e958fbaa662b923d8c6cef42b1
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-27 22:07:27 -0700

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   has_secure_password
+  validates :username, length: { in: 3..30 }, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false }
+  validates :password, length: { minimum: 6 }, presence: true
+  
+  validates :password, :allow_blank, on: :update
+  before_save { self.email.downcase! }
 end
