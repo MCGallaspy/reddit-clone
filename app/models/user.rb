6d3c3015624c1dccb4873b2b07d8fb0ac60e87b1
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-27 22:38:59 -0700

    User's username shouldn't contain any whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   has_secure_password
-  validates :username, length: { in: 3..30 }, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, length: { in: 3..30 }, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\S]+\z/ }
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false }
   validates :password, length: { minimum: 6 }, presence: true
   
   validates :password, :allow_blank, on: :update
   before_save { self.email.downcase! }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MINCOST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
