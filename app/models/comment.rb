a46b3d074d45b3e87fd45e56b0db9d8ae701c5cf
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-28 20:02:21 -0700

    has_many relation with name as children creates conflict, user identifier comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
   belongs_to :user, inverse_of: :comments
-  has_many :children, as: :parent
+  has_many :comments, as: :parent
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :user, inverse_of: :posts
-  has_many :children, as: :parent
+  has_many :comments, as: :parent
 end
