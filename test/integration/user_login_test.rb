a6a1d3e472fefce7fbf16577a57771df76e66f24
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-30 15:34:03 -0700

    Add login integration tests

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:mike)
+  end
+
+  test "login with invalid information" do
+    get root_path
+    assert_template 'layouts/application'
+    post login_path, session: { username: "    ", password: " " }
+    assert_template 'layouts/application'
+    assert_not flash.empty?, "Invalid credentials should give error"
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid credentials followed by logout" do
+    get root_path
+    assert_template 'layouts/application'
+    post login_path, session: { username: @user.username, password: "MyString" }
+    assert is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'layouts/application'
+    assert_select "form[action=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    # Now logout
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+    assert_select "form[action=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
