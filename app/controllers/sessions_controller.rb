17b4adeb3d55471f85bd01c4c8fa5ce0142272fd
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-10-30 17:01:30 -0700

    Add session login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,23 @@
 class SessionsController < ApplicationController
   def create
-    render text: "", layout: 'layouts/application'
+    username, password = login_params
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to root_path
+    else
+      flash.now[:login_error] = "incorrect password"
+      render text: "", layout: 'layouts/application'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
+
+  private
+    def login_params
+      params.require(:session).permit(:username, :password)
+    end
 end
