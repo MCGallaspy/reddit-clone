120e52c6ba787b65fe64e787c9eaa835ae38df29
Michael Gallaspy <gallaspy.michael@gmail.com>
2014-11-05 18:07:41 -0800

    Fix test logic

--- test/integration/create_and_edit_posts_test.rb
+++ test/integration/create_and_edit_posts_test.rb
@@ -3,18 +3,19 @@ require 'test_helper'
 class CreateAndEditPostsTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:mike)
+    log_in_as @user, password: "MyString"
   end
 
   test "create new self post" do
     get new_post_path, post_type: :self_post
-    assert_select "form[id=?]", :self_post_form
+    assert_select "form[id=?]", :self_post_form 
     assert_select "form[id=?]", :link_post_form, count: 0
     assert_difference 'Post.count' do
-      post_via_redirect posts_path, post: { title: "test title",
-                                            self_text: "A test post.",
-                                            user: @user,
-                                            link: nil,
-                                            is_self_post: true }
+      post posts_path, post: { title: "test title",
+                               self_text: "A test post.",
+                               user_id: @user.id,
+                               link: nil,
+                               is_self_post: true }
     end
     @post = Post.order('created_at').last
     assert_redirected_to post_path @post
@@ -25,11 +26,11 @@ class CreateAndEditPostsTest < ActionDispatch::IntegrationTest
     assert_select "form[id=?]", :link_post_form
     assert_select "form[id=?]", :self_post_form, count: 0
     assert_difference 'Post.count' do
-      post_via_redirect posts_path, post: { title: "test title",
-                                            self_text: nil,
-                                            user: @user,
-                                            link: "http://www.google.com",
-                                            is_self_post: true }
+      post posts_path, post: { title: "test title",
+                               self_text: nil,
+                               user_id: @user.id,
+                               link: "http://www.google.com",
+                               is_self_post: false }
     end
     @post = Post.order('created_at').last
     assert_redirected_to post_path @post
